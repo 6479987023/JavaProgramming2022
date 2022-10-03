f1d9f9f5888b248d345cb2d23960a82cd183900d
Julaiti Ailaiti <hayrat2306@gmail.com>
2022-10-03 15:28:31 -0400

    first commit

---
+++ src/day2/HelloWorld.java
@@ -0,0 +1,12 @@
+package day2;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world" +
+                "fdf ");
+        System.out.print("lol");
+        System.out.println("lol");
+
+    }
+}
