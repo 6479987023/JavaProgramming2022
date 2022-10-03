379e2e6967e4c62fdc728373041162cc79e3d548
Julaiti Ailaiti <hayrat2306@gmail.com>
2022-10-03 15:44:13 -0400

    HelloCybertek

--- src/day2/HelloCybertek.java
+++ src/day2/HelloCybertek.java
@@ -5,6 +5,9 @@ public class HelloCybertek {
     public static void main(String[] args){
 
         System.out.println("Hello Cybertek");
+        System.out.println("Hello Cybertek");
+        System.out.println("Hello Cybertek");
+        System.out.println("Hello Cybertek");
 
     }
 
